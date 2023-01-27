ffa599f974f7a472b16126c8cb3d65dcb3dea8f9
Kaylen <j.anderwald@hotmail.com>
2023-01-27 17:29:45 +1100

    Update password_options.py

--- password_options.py
+++ password_options.py
@@ -1,29 +1,43 @@
 import random
 import string
-
+# ---------------------------------------------------------------------------------------------------------------------------------------
+# This class allows the user to create a password that will be forgotten after user. This password will increase in complexity depending
+# on the option the user selects.
+# Weak passwords only user lowercase ascii characters.
+# Medium strength user lowercase ascii characters and numbers.
+# Strong passwords will utilise ascii characters as well as special characters.
+# ---------------------------------------------↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓---------------------------------------------
 class Password_options:
 
     def weak_password(length):
-        weak_selection = string.ascii_lowercase     #here we are using string.ascii_lowercase, this is to allow the random function to select from alphabetical lowercase only
-        result_easy_string = ''.join(random.choice(weak_selection) for i in range(length))     #here we are taking a randomly selected number of characters and joining them together,
-        print("Your weak password containing", length, "characters is:", result_easy_string)   #The for loop will then repeat this selection for the total of the number selected by the user.
+        weak_selection = string.ascii_lowercase
+        result_easy_string = ''.join(random.choice(weak_selection) for i in range(length))
+        print("Your weak password containing", length, "characters is:", result_easy_string)
 
     def medium_password(length):
-        medium_password = string.ascii_lowercase + string.digits     #The medium strength uses alphanumeric values. It does the same thing as the function above
-        result_hard_string = ''.join(random.choice(medium_password) for i in range(length))     #but it selects from a numeric value as well.
+        medium_password = string.ascii_lowercase + string.digits
+        result_hard_string = ''.join(random.choice(medium_password) for i in range(length))
         print("Your strong password containing", length, "characters is:", result_hard_string)
 
     def strong_password(length):
-        strong_password = string.ascii_lowercase + string.digits + string.punctuation     #In the strong password section, characters are also available from the special characters catagory.
+        strong_password = string.ascii_lowercase + string.digits + string.punctuation
         result_hard_string = ''.join(random.choice(strong_password) for i in range(length))
         print("Your strong password containing", length, "characters is:", result_hard_string)
 
+# ---------------------------------------------------------------------------------------------------------------------------------------
+# This class will create a password for the user and then save it. This password will increase in complexity depending
+# on the option the user selects.
+# Weak passwords only user lowercase ascii characters.
+# Medium strength user lowercase ascii characters and numbers.
+# Strong passwords will utilise ascii characters as well as special characters.
+# The program will then write the password to a file
+# ---------------------------------------------↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓---------------------------------------------
 class Password_save_options:
 
     def weak_password(length):
-        weak_selection = string.ascii_lowercase     #here we are using string.ascii_lowercase, this is to allow the random function to select from alphabetical lowercase only
-        result_easy_string = ''.join(random.choice(weak_selection) for i in range(length))     #here we are taking a randomly selected number of characters and joining them together,
-        print("Your weak password containing", length, "characters is:", result_easy_string)   #The for loop will then repeat this selection for the total of the number selected by the user.
+        weak_selection = string.ascii_lowercase
+        result_easy_string = ''.join(random.choice(weak_selection) for i in range(length))
+        print("Your weak password containing", length, "characters is:", result_easy_string)
         f = open("user_passwords.txt", "a")
         f.write(result_easy_string + "\n")
         f.close()
@@ -43,7 +57,14 @@ class Password_save_options:
         f = open("user_passwords.txt", "a")
         f.write(result_hard_string + "\n")
         f.close()
-
+# ---------------------------------------------------------------------------------------------------------------------------------------
+# This class will create a password for the user and then save it and allow the user to save a service i.e. a website.
+# This password will increase in complexity depending on the option the user selects.
+# Weak passwords only user lowercase ascii characters.
+# Medium strength user lowercase ascii characters and numbers.
+# Strong passwords will utilise ascii characters as well as special characters.
+# The program will then write the password to a file
+# ---------------------------------------------↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓---------------------------------------------
 class Password_save_with_service_options:
 
     def weak_password_with_service(length, service):
